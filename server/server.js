49cd5ecb8e03bbcac7d8abb75da6aeb6a9ecd968
Aimee Wang <aimeewang339@gmail.com>
2025-11-25 20:04:07 -0600

    Shop images

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 // Connect to MongoDB (Atlas)
 connectDB();
